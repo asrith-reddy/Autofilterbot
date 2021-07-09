556ea459fad1cfc981e3c58aa323fb364788b1e5
asrith-reddy <76733972+asrith-reddy@users.noreply.github.com>
2021-07-09 17:32:39 +0530

    Update commands.py

--- bot/plugins/commands.py
+++ bot/plugins/commands.py
@@ -50,7 +50,7 @@ async def start(bot, update):
             await bot.send_video(
                 chat_id=update.chat.id,
                 video = file_id,
-                caption = caption",
+                caption = caption,
                 parse_mode="html",
                 reply_markup=InlineKeyboardMarkup(
                     [
